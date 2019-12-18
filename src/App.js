7ec4944a522cdb8ef3aad6156b53a3cc7c197670
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-18 15:05:56 +0100

    playing with positioning the strollers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { useRef, useEffect, useState } from "react";
 import { TweenMax, Power3 } from "gsap";
+import leftArrow from "./assets/arrow-forward.svg";
+import rightArrow from "./assets/arrow-forward.svg";
 import "./App.scss";
+import "reset-css";
+
+const details = [
+  {
+    name: "Buggy XS",
+    subtitle: "Compact on the go",
+    image: `${require("./assets/buggyxs.png")}`
+  },
+  {
+    name: "Harvey²",
+    subtitle: "All Terrain",
+    image: `${require("./assets/harvey2.png")}`
+  }
+];
 
 function App() {
-  let app = useRef(null);
-
   return (
-    <div className="App" ref={el => (app = el)}>
+    <div className="App">
+      <div className="slider-container">
+
+        <div className="arrows left">
+          <img src={leftArrow} alt="left arrow" />
+        </div>
+
+        <div className="inner">
+          <div className="strollers">
+            <img className="stroller1" src={details[0].image} alt={details[0].name}/>
+            <img className="stroller2" src={details[1].image} alt={details[1].name}/>
+          </div>
+        </div>
+
+        <div className="arrows right">
+          <img src={rightArrow} alt="right arrow" />
+        </div>
+
+      </div>
     </div>
   );
 }
