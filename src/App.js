59d0352d96e8124b3a63dc27bc4d4d8401b8e9f7
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-18 18:25:07 +0100

    it works! image infinitely scrolls from both directions, bug with needing to wait till animation is completely finished first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import { TweenMax, Power3 } from "gsap";
+import { TweenLite, Power3 } from "gsap";
 import leftArrow from "./assets/arrow-forward.svg";
 import rightArrow from "./assets/arrow-forward.svg";
 import "./App.scss";
@@ -19,25 +19,88 @@ const details = [
 ];
 
 function App() {
+  let strollers = useRef(null);
+
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false
+  });
+
+  useEffect(() => {
+    console.log(strollers.children);
+  }, []);
+
+  // Stroller Image transition
+
+  const slideLeft = (rightIndex, leftIndex, duration) => {
+    TweenLite.set(strollers.children[rightIndex], { css: { left: "130vw" } });
+    TweenLite.to(strollers.children[rightIndex], duration, {
+      css: { left: "30vw" },
+      ease: Power3.easeInOut
+    });
+    TweenLite.to(strollers.children[leftIndex], duration, {
+      css: { left: "-130vw" },
+      ease: Power3.easeInOut
+    });
+  }
+
+  const slideRight = (rightIndex, leftIndex, duration) => {
+    TweenLite.set(strollers.children[leftIndex], { css: { left: "-130vw" } });
+    TweenLite.to(strollers.children[rightIndex], duration, {
+      css: { left: "130vw" },
+      ease: Power3.easeInOut
+    });
+    TweenLite.to(strollers.children[leftIndex], duration, {
+      css: { left: "30vw" },
+      ease: Power3.easeInOut
+    });
+  }
+
+  const nextSlide = () => {
+    if (strollers.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true });
+      slideLeft(1, 0, 1.4)
+    } else if (strollers.children[1].classList.contains("active")) {
+      setState({ isActive1: true, isActive2: false });
+      slideLeft(0, 1, 1.4)
+    }
+  };
+
+  const previousSlide = () => {
+    if (strollers.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true });
+      slideRight(0, 1, 1.4)
+    } else if (strollers.children[1].classList.contains("active")) {
+      setState({ isActive1: true, isActive2: false });
+      slideRight(1, 0, 1.4)
+    }
+  };
+
   return (
     <div className="App">
       <div className="slider-container">
-
-        <div className="arrows left">
+        <div onClick={previousSlide} className="arrows left">
           <img src={leftArrow} alt="left arrow" />
         </div>
 
         <div className="inner">
-          <div className="strollers">
-            <img className="stroller1" src={details[0].image} alt={details[0].name}/>
-            <img className="stroller2" src={details[1].image} alt={details[1].name}/>
+          <div ref={el => (strollers = el)} className="strollers">
+            <img
+              className={state.isActive1 ? "stroller1 active" : "stroller1"}
+              src={details[0].image}
+              alt={details[0].name}
+            />
+            <img
+              className={state.isActive2 ? "stroller2 active" : "stroller2"}
+              src={details[1].image}
+              alt={details[1].name}
+            />
           </div>
         </div>
 
-        <div className="arrows right">
+        <div onClick={nextSlide} className="arrows right">
           <img src={rightArrow} alt="right arrow" />
         </div>
-
       </div>
     </div>
   );
