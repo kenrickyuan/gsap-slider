14ab925a84a6a3a0c3c1c5e85f97c2190b0e094a
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-19 20:55:20 +0100

    really refactored a lot this time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { TweenLite, Power3, Power0 } from "gsap";
+import { TweenLite, Power3 } from "gsap";
 import arrowImg from "./assets/arrow.svg";
 import "./App.scss";
 import "reset-css";
@@ -7,21 +7,29 @@ import "reset-css";
 const details = [
   {
     name: "Buggy XS",
+    nameLeft: 0, // only input number, it will be in vw
     subtitle: "Compact on the go",
-    image: `${require("./assets/buggyxs.png")}`,
+    subLeft: 28, // only input number, it will be in vw
+    img: `${require("./assets/buggyxs.png")}`,
+    imgLeft: 36, // only input number, it will be in vw
     bgColor: "#95b1b9",
     shapeColor: "#b4cad0",
-    shapeWidth: "51vw",
-    shapeHeight: "35vw"
+    shapeWidth: '51vw',
+    shapeHeight: '35vw',
+    shapeBorderRadius: "0%"
   },
   {
     name: "Harvey²",
+    nameLeft: 6, // only input number, it will be in vw
     subtitle: "All Terrain",
-    image: `${require("./assets/harvey2.png")}`,
+    subLeft: 37, // only input number, it will be in vw
+    img: `${require("./assets/harvey2.png")}`,
+    imgLeft: 38, // only input number, it will be in vw
     bgColor: "#958f96",
     shapeColor: "#706971",
-    shapeWidth: "44vw",
-    shapeHeight: "44vw"
+    shapeWidth: '44vw',
+    shapeHeight: '44vw',
+    shapeBorderRadius: "50%"
   }
 ];
 
@@ -29,10 +37,10 @@ function App() {
   let strollers = useRef(null);
   let container = useRef(null);
   let subtitles = useRef(null);
-  let heading = useRef(null);
+  let headings = useRef(null);
   let shape = useRef(null);
-  let leftArrow = useRef(null);
-  let rightArrow = useRef(null);
+  // let leftArrow = useRef(null);
+  // let rightArrow = useRef(null);
 
   const [state, setState] = useState({
     isActive1: true,
@@ -40,90 +48,60 @@ function App() {
   });
 
   // Stroller Image transition
-  // Input starting stroller's index, ending stroller's index, starting stroller's left, ending stroller's left, starting rotation, ending rotation, duration
-  const slideImgLeft = (
-    startIndex,
-    endIndex,
-    startLeft,
-    endLeft,
-    startRotation,
-    endRotation,
-    duration
-  ) => {
+  // Input starting stroller's index, ending stroller's index, starting rotation, ending rotation, duration
+  const slideImgLeft = (startIndex, endIndex, startRotation, endRotation, duration) => {
     TweenLite.set(strollers.children[endIndex], {
-      css: { left: `${100 + endLeft}vw`, rotation: startRotation }
+      css: { left: `${100 + details[endIndex].imgLeft}vw`, rotation: startRotation }
     });
     TweenLite.to(strollers.children[endIndex], duration, {
-      css: { left: `${endLeft}vw`, rotation: endRotation },
+      css: { left: `${details[endIndex].imgLeft}vw`, rotation: endRotation },
       ease: Power3.easeInOut
     });
     TweenLite.to(strollers.children[startIndex], duration, {
-      css: { left: `${-100 - startLeft}vw`, rotation: startRotation },
+      css: { left: `${-100 - details[startIndex].imgLeft}vw`, rotation: startRotation },
       ease: Power3.easeInOut
     });
   };
 
-  const slideImgRight = (
-    startIndex,
-    endIndex,
-    startLeft,
-    endLeft,
-    startRotation,
-    endRotation,
-    duration
-  ) => {
+  const slideImgRight = (startIndex, endIndex, startRotation, endRotation, duration) => {
     TweenLite.set(strollers.children[endIndex], {
-      css: { left: `${-100 - endLeft}vw`, rotation: startRotation }
+      css: { left: `${-100 - details[endIndex].imgLeft}vw`, rotation: startRotation }
     });
     TweenLite.to(strollers.children[endIndex], duration, {
-      css: { left: `${endLeft}vw`, rotation: endRotation },
+      css: { left: `${details[endIndex].imgLeft}vw`, rotation: endRotation },
       ease: Power3.easeInOut
     });
     TweenLite.to(strollers.children[startIndex], duration, {
-      css: { left: `${100 + startLeft}vw`, rotation: startRotation },
+      css: { left: `${100 + details[startIndex].imgLeft}vw`, rotation: startRotation },
       ease: Power3.easeInOut
     });
   };
 
   // Subtitle transition
-  const slideSubLeft = (
-    startIndex,
-    endIndex,
-    newLeft,
-    newTop,
-    currentLeft,
-    duration
-  ) => {
+  const slideSubLeft = (startIndex, endIndex, duration) => {
     TweenLite.set(subtitles.children[endIndex], {
-      css: { left: `${100 + newLeft}vw`, top: `${newTop}vh` }
+      css: { left: `${100 + details[endIndex].subLeft}vw` }
     });
     TweenLite.to(subtitles.children[endIndex], duration, {
-      css: { left: `${newLeft}vw` },
+      css: { left: `${details[endIndex].subLeft}vw` },
       ease: Power3.easeInOut
     });
     TweenLite.to(subtitles.children[startIndex], duration, {
-      css: { left: `${-100 - currentLeft}vw` },
+      css: { left: `${-100 - details[startIndex].subLeft}vw` },
       ease: Power3.easeInOut
     });
   };
 
-  const slideSubRight = (
-    startIndex,
-    endIndex,
-    newLeft,
-    newTop,
-    currentLeft,
-    duration
-  ) => {
+  const slideSubRight = (startIndex, endIndex, duration) => {
     TweenLite.set(subtitles.children[endIndex], {
-      css: { left: `${-100 - newLeft}vw`, top: `${newTop}vh` }
+      css: { left: `${-100 - details[endIndex].subLeft}vw` }
     });
     TweenLite.to(subtitles.children[endIndex], duration, {
-      css: { left: `${newLeft}vw` },
+      css: { left: `${details[endIndex].subLeft}vw` },
       ease: Power3.easeInOut
     });
     TweenLite.to(subtitles.children[startIndex], duration, {
-      css: { left: `${100 + currentLeft}vw` },
+      css: { left: `${100 + details[startIndex].subLeft}vw` },
       ease: Power3.easeInOut
     });
   };
@@ -137,46 +115,67 @@ function App() {
   };
 
   // Shape transition
-  const shapeChange = (targetColor, targetWidth, targetHeight, targetBorderRadius, targetRotation, duration) => {
+  const shapeChange = (targetIndex, targetRotation, duration) => {
     TweenLite.to(shape, duration, {
-      css: { "background-color": targetColor, "width":targetWidth, "height":targetHeight, "border-radius": targetBorderRadius, rotation: `+= ${targetRotation}`},
+      css: {
+        "background-color": details[targetIndex].shapeColor,
+        width: details[targetIndex].shapeWidth,
+        height: details[targetIndex].shapeHeight,
+        "border-radius": details[targetIndex].shapeBorderRadius,
+        rotation: `+= ${targetRotation}`
+      },
       ease: Power3.easeInOut
     });
   };
 
-  // Heading Changing transition (for testing purposes)
-  const headingChange = targetHeading => {
-    heading.innerHTML = targetHeading;
+  // Heading transition
+  const slideHeadLeft = (startIndex, endIndex, duration) => {
+    TweenLite.set(headings.children[endIndex], {
+      css: { left: `${100 + details[endIndex].nameLeft}vw` }
+    });
+    TweenLite.to(headings.children[endIndex], duration, {
+      css: { left: `${details[endIndex].nameLeft}vw` },
+      ease: Power3.easeInOut
+    });
+    TweenLite.to(headings.children[startIndex], duration, {
+      css: { left: `${-100 - details[startIndex].nameLeft}vw` },
+      ease: Power3.easeInOut
+    });
   };
 
-  // Temporary solution to prevent animation bugs when clicking arrows before GSAP transforms are completely over
-  const disableArrow = arrow => {
-    arrow.classList.add("disabled");
-  };
+  // // Heading Changing transition (for testing purposes)
+  // const headingChange = targetHeading => {
+  //   heading.innerHTML = targetHeading;
+  // };
 
-  const enableArrow = () => {
-    console.log(rightArrow);
-    // rightArrow.classList.remove("disabled");
-  };
+  // // Temporary solution to prevent animation bugs when clicking arrows before GSAP transforms are completely over
+  // const disableArrow = arrow => {
+  //   arrow.classList.add("disabled");
+  // };
+
+  // const enableArrow = () => {
+  //   console.log(rightArrow);
+  //   // rightArrow.classList.remove("disabled");
+  // };
 
   const nextSlide = () => {
     if (strollers.children[0].classList.contains("active")) {
       // disableArrow(rightArrow);
       setState({ isActive1: false, isActive2: true });
-      slideImgLeft(0, 1, 38, 36, 65, 10, 1.6);
-      slideSubLeft(0, 1, 37, 49, 28, 1.6);
+      slideImgLeft(0, 1, 65, 10, 1.6);
+      slideHeadLeft(0, 1, 1.6);
+      slideSubLeft(0, 1, 1.6);
       colorChange(details[1].bgColor, 1.6);
-      headingChange(details[1].name);
-      shapeChange(details[1].shapeColor, details[1].shapeWidth, details[1].shapeHeight, "50%", -90, 1.6);
+      shapeChange(1, -90, 1.6);
       // setTimeout(enableArrow, 1600);
     } else if (strollers.children[1].classList.contains("active")) {
       // disableArrow(rightArrow);
       setState({ isActive1: true, isActive2: false });
-      slideImgLeft(1, 0, 36, 38, 65, 10, 1.6);
-      slideSubLeft(1, 0, 28, "", 37, 1.6);
+      slideImgLeft(1, 0, 65, 10, 1.6);
+      slideHeadLeft(1, 0, 1.6);
+      slideSubLeft(1, 0, 1.6);
       colorChange(details[0].bgColor, 1.6);
-      headingChange(details[0].name);
-      shapeChange(details[0].shapeColor, details[0].shapeWidth, details[0].shapeHeight, "0%", -90, 1.6);
+      shapeChange(0, -90, 1.6);
       // setTimeout(enableArrow, 1600);
     }
   };
@@ -184,84 +183,63 @@ function App() {
   const previousSlide = () => {
     if (strollers.children[0].classList.contains("active")) {
       setState({ isActive1: false, isActive2: true });
-      slideImgRight(0, 1, 38, 36, 65, 10, 1.6);
-      slideSubRight(0, 1, 37, 49, 28, 1.6);
+      slideImgRight(0, 1, 65, 10, 1.6);
+      slideSubRight(0, 1, 1.6);
       colorChange(details[1].bgColor, 1.6);
-      headingChange(details[1].name);
-      shapeChange(details[1].shapeColor, details[1].shapeWidth, details[1].shapeHeight, "50%", 90, 1.6);
+      // headingChange(details[1].name);
+      shapeChange(1, 90, 1.6);
     } else if (strollers.children[1].classList.contains("active")) {
       setState({ isActive1: true, isActive2: false });
-      slideImgRight(1, 0, 36, 38, 65, 10, 1.6);
-      slideSubRight(1, 0, 28, "", 37, 1.6);
+      slideImgRight(1, 0, 65, 10, 1.6);
+      slideSubRight(1, 0, 1.6);
       colorChange(details[0].bgColor, 1.6);
-      headingChange(details[0].name);
-      shapeChange(details[0].shapeColor, details[0].shapeWidth, details[0].shapeHeight, "0%", 90, 1.6);
+      // headingChange(details[0].name);
+      shapeChange(0, 90, 1.6);
     }
   };
 
   return (
     <div className="App">
       <div ref={el => (container = el)} className="slider-container">
-        <div
-          ref={el => (leftArrow = el)}
-          onClick={previousSlide}
-          className="arrows left"
-        >
+        <div onClick={previousSlide} className="arrows left">
           <img src={arrowImg} alt="left arrow" />
         </div>
 
+        <div className="shape-container">
+          <span ref={el => (shape = el)} className="shape"></span>
+        </div>
+        <div className="stroller-details-container">
 
-          <div className="shape-container">
-            <span ref={el => (shape = el)} className="shape"></span>
-          </div>
-          <div className="stroller-details-container">
-            <div className="stroller-name">
-              <h1 ref={el => (heading = el)}>{details[0].name}</h1>
-              <div ref={el => (subtitles = el)} className="subtitles">
-                <p
-                  className={
-                    state.isActive1
-                      ? "stroller-subtitle active"
-                      : "stroller-subtitle"
-                  }
-                >
-                  {details[0].subtitle}
-                </p>
-                <p
-                  className={
-                    state.isActive2
-                      ? "stroller-subtitle stroller-subtitle2 active"
-                      : "stroller-subtitle stroller-subtitle2"
-                  }
-                >
-                  {details[1].subtitle}
-                </p>
-              </div>
+            <div ref={el => (headings = el)} className="headings">
+              <h1 className={state.isActive1 ? "heading active" : "heading"}>{details[0].name}</h1>
+              <h1 className={state.isActive2 ? "heading heading2 active" : "heading heading2"}>{details[1].name}</h1>
             </div>
 
+          <div ref={el => (subtitles = el)} className="subtitles">
+            <p className={state.isActive1 ? "stroller-subtitle active" : "stroller-subtitle"}>{details[0].subtitle}</p>
+            <p
+              className={
+                state.isActive2 ? "stroller-subtitle stroller-subtitle2 active" : "stroller-subtitle stroller-subtitle2"
+              }
+            >
+              {details[1].subtitle}
+            </p>
+          </div>
           <div ref={el => (strollers = el)} className="strollers">
             <img
               className={state.isActive1 ? "stroller active" : "stroller"}
-              src={details[0].image}
+              src={details[0].img}
               alt={details[0].name}
             />
             <img
-              className={
-                state.isActive2
-                  ? "stroller stroller2 active"
-                  : "stroller stroller2"
-              }
-              src={details[1].image}
+              className={state.isActive2 ? "stroller stroller2 active" : "stroller stroller2"}
+              src={details[1].img}
               alt={details[1].name}
             />
           </div>
         </div>
 
-        <div
-          ref={el => (rightArrow = el)}
-          onClick={nextSlide}
-          className="arrows right"
-        >
+        <div onClick={nextSlide} className="arrows right">
           <img src={arrowImg} alt="right arrow" />
         </div>
       </div>
