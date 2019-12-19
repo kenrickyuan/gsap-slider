85488183943857e127cb443a865393aeeb47d3b5
Kenrick Yuan <kenrickyuan@gmail.com>
2019-12-19 22:39:48 +0100

    file structure changed to better isolate component

---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,30 @@
+export const sliderData = [
+  {
+    name: "Buggy XS",
+    nameLeft: 12, // only input number, it will be in vw
+    nameTop: -2,
+    subtitle: "Compact on the go",
+    subLeft: 28, // only input number, it will be in vw
+    img: `${require("../../assets/buggyxs.png")}`,
+    imgLeft: 36, // only input number, it will be in vw
+    bgColor: "#95b1b9",
+    shapeColor: "#b4cad0",
+    shapeWidth: "51vw",
+    shapeHeight: "35vw",
+    shapeBorderRadius: "0%"
+  },
+  {
+    name: "Harvey²",
+    nameLeft: 18, // only input number, it will be in vw
+    nameTop: -21,
+    subtitle: "All Terrain",
+    subLeft: 37, // only input number, it will be in vw
+    img: `${require("../../assets/harvey2.png")}`,
+    imgLeft: 38, // only input number, it will be in vw
+    bgColor: "#958f96",
+    shapeColor: "#706971",
+    shapeWidth: "44vw",
+    shapeHeight: "44vw",
+    shapeBorderRadius: "50%"
+  }
+];
